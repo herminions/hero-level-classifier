efaeecc40ccf937b99fe525e0eabff756380a687
herminions <hermesfilippi@gmail.com>
2024-01-14 14:35:32 -0300

    refactor: reoganized the code

--- level-classifier.js
+++ level-classifier.js
@@ -5,6 +5,7 @@ let levels = ["ferro" , "bronze" , "prata" , "ouro" , "platina" , "ascendente" ,
 for(heroXP = 0 ; heroXP < 11000; heroXP += 1001){
     if(heroXP <= 1000){
     console.log("O herói de nome " + heroName + " está no nível " + levels[0] )
+<<<<<<< HEAD
     } else if(heroXP <= 2000){
         console.log("O herói de nome " + heroName + " está no nível " + levels[1] )
     }  
@@ -26,4 +27,21 @@ for(heroXP = 0 ; heroXP < 11000; heroXP += 1001){
     else{
         console.log("O herói de nome " + heroName + " está no nível " + levels[7] )
     }
+=======
+} else if(heroXP <= 2000){
+    console.log("O herói de nome " + heroName + " está no nível " + levels[1] )
+} else if(heroXP <= 5000){
+    console.log("O herói de nome " + heroName + " está no nível " + levels[2] )
+} else if(heroXP <= 7000){
+    console.log("O herói de nome " + heroName + " está no nível " + levels[3] )
+} else if(heroXP <= 8000){
+    console.log("O herói de nome " + heroName + " está no nível " + levels[4] )
+} else if(heroXP <= 9000){
+    console.log("O herói de nome " + heroName + " está no nível " + levels[5] )
+} else if(heroXP <= 10000){
+    console.log("O herói de nome " + heroName + " está no nível " + levels[6] )
+} else{
+    console.log("O herói de nome " + heroName + " está no nível " + levels[7] )
+}
+>>>>>>> dd62f2e0ed32925d077de8391b5377968b8f8b60
 }
